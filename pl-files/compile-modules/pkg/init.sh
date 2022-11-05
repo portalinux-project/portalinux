17ac1d29ee838eb23dab21bc78d36861c4d8dbd2
raisinware <102261580+raisinware@users.noreply.github.com>
2022-11-04 18:28:44 -0700

    remove extra #'s

--- pl-files/compile-modules/pkg/init.sh
+++ pl-files/compile-modules/pkg/init.sh
@@ -1,4 +1,4 @@
-## SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 _init(){
 	if [ -d $build ]; then
--- pl-files/compile-modules/runtime_calc.sh
+++ pl-files/compile-modules/runtime_calc.sh
@@ -1,4 +1,4 @@
-## SPDX-License-Identifier: GPL-2.0-or-later
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 start_time=""
 end_time=""
