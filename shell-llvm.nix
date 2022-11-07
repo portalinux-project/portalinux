bcfd8a4fa78a0d3246a4c56f408cbc45d60ddc4a
raisinware <102261580+raisinware@users.noreply.github.com>
2022-11-07 13:36:59 -0800

    Create shell-llvm.nix

---
+++ shell-llvm.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell.override { stdenv = pkgs.llvmPackages_14.libcxxStdenv; } {
+	name = "pl-nix-llvm";
+
+	shellHook=''
+	export PATH="$HOME/cross/bin:$PATH" # add default toolchain location to path
+	echo "PortaLinux nix-shell LLVM Development Environment"
+	'';
+
+	hardeningDisable = [ "format" ]; # fixes errors trying to build the gcc toolchain
+	nativeBuildInputs = [
+		#pkgs.busybox
+		pkgs.cacert
+		pkgs.wget
+		pkgs.which
+		pkgs.unixtools.script
+		pkgs.rsync
+		pkgs.gnumake
+		pkgs.flex
+		pkgs.bison
+		pkgs.gawk
+
+		# kernel build deps
+		pkgs.ncurses
+		pkgs.libressl
+		pkgs.bc
+		pkgs.perl
+		pkgs.kmod
+	];
+}
