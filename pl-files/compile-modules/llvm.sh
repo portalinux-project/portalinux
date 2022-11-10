f42c42fa109704e3ac5028b6219e39bbca48df54
raisinware <102261580+raisinware@users.noreply.github.com>
2022-11-09 23:11:52 -0800

    C++ SUPPORT!!!!

--- pl-files/compile-modules/llvm.sh
+++ pl-files/compile-modules/llvm.sh
@@ -11,7 +11,8 @@ set(triple "$compile_target")
 set(CMAKE_ASM_COMPILER_TARGET \${triple})
 set(CMAKE_C_COMPILER_TARGET \${triple})
 set(CMAKE_CXX_COMPILER_TARGET \${triple})
-set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld --rtlib=compiler-rt")
+set(CMAKE_EXE_LINKER_FLAGS "--ld-path='$toolchain_prefix/bin/ld.lld' --rtlib=compiler-rt")
+set(CMAKE_SHARED_LINKER_FLAGS \${CMAKE_EXE_LINKER_FLAGS})
 
 set(CMAKE_C_COMPILER "$toolchain_prefix/bin/clang")
 set(CMAKE_CXX_COMPILER "$toolchain_prefix/bin/clang++")
@@ -53,9 +54,10 @@ compile_toolchain(){
 						-DLLVM_LINK_LLVM_DYLIB=on -DCLANG_LINK_CLANG_DYLIB=on -DLLVM_ENABLE_PROJECTS=clang\;lld -DLLVM_ENABLE_RUNTIMES='' -DLLVM_HAVE_LIBXAR=0"
 		_exec "Compiling LLVM...\n" "cmake --build build/toolchain -j$threads" no-silent
 		_exec "Installing LLVM" "cmake --install build/toolchain --strip"
+		# TODO: compress headers and static libs created by LLVM
 	fi
 
-	# libc headers
+	# musl libc headers
 	if [ ! -r "$sysroot/include/stdio.h" ]; then
 		cd "$libc_dir"
 		_exec "Installing musl headers" "make ARCH=$arch prefix=$sysroot install-headers"
@@ -65,14 +67,17 @@ compile_toolchain(){
 	if [ ! -r "$sysroot/lib/linux/libclang_rt.builtins-$linux_arch.a" ]; then
 		cd "$llvm_dir"
 		rm -rf "build/builtins"
-		_exec "Configuring compiler-rt builtins" "cmake -S ./runtimes -B build/builtins $cmake_cross_flags -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
-						-DLLVM_ENABLE_RUNTIMES=compiler-rt -DCOMPILER_RT_BUILD_LIBFUZZER=OFF -DCOMPILER_RT_BUILD_MEMPROF=OFF -DCOMPILER_RT_BUILD_ORC=OFF \
-						-DCOMPILER_RT_BUILD_PROFILE=OFF -DCOMPILER_RT_BUILD_SANITIZERS=OFF -DCOMPILER_RT_BUILD_XRAY=OFF -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON"
-		_exec "Compiling compiler-rt builtins...\n" "cmake --build build/builtins -j$threads" no-silent
-		_exec "Installing LLVM" "cmake --install build/builtins --strip"
+		_exec "Configuring compiler-rt builtins" "cmake -S ./compiler-rt -B build/builtins $cmake_cross_flags -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
+						-DCOMPILER_RT_BUILD_LIBFUZZER=OFF -DCOMPILER_RT_BUILD_MEMPROF=OFF -DCOMPILER_RT_BUILD_ORC=OFF -DCOMPILER_RT_BUILD_PROFILE=OFF \
+						-DCOMPILER_RT_BUILD_SANITIZERS=OFF -DCOMPILER_RT_BUILD_XRAY=OFF -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON"
+		_exec "Compiling compiler-rt builtins" "cmake --build build/builtins -j$threads"
+		_exec "Installing compiler-rt builtins" "cmake --install build/builtins --strip"
+		# fix linking errors
+		for i in begin end; do ln -sf "./linux/clang_rt.crt$i-$linux_arch.o" "$sysroot/lib/crt"$i"S.o"; done
+		ln -sf "../../../../../$compile_target/lib/linux/libclang_rt.builtins-$linux_arch.a" "$toolchain_prefix/lib/clang/14.0.6/lib/linux/libclang_rt.builtins-i386.a"
 	fi
 
-	# libc
+	# musl libc
 	if [ ! -r "$sysroot/lib/libc.so" ]; then
 		cd "$libc_dir"
 		_exec "Configuring musl libc" "ARCH=$arch CC='$cross_cc' LIBCC=$sysroot/lib/linux/libclang_rt.builtins-$linux_arch.a ./configure --prefix=$sysroot --host=$common_flags"
@@ -80,26 +85,36 @@ compile_toolchain(){
 		_exec "Installing musl libc" "make AR='$toolchain_prefix/bin/llvm-ar' RANLIB='$toolchain_prefix/bin/llvm-ranlib' install"
 	fi
 
-	# # libgcc-shared (musl-only)
-	# if [ ! -r "$sysroot/$libdir/libgcc_s.so" ]; then
-	# 	cd "$gcc_dir/build"
-	# 	_exec "Cleaning libgcc" "make -C $compile_target/libgcc distclean"
-	# 	_exec "Compiling libgcc-shared" "make enable_shared=yes -j$threads all-target-libgcc"
-	# 	_exec "Installing libgcc" "make install-strip-target-libgcc"
-	# fi
+	# libatomic
+	if [ ! -r "$sysroot/lib/linux/libclang_rt.atomic-$linux_arch.so" ]; then
+		cd "$llvm_dir"
+		rm -rf "build/atomic"
+		_exec "Configuring libatomic" "cmake -S ./compiler-rt -B build/atomic $cmake_cross_flags -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
+						-DCOMPILER_RT_BUILD_LIBFUZZER=OFF -DCOMPILER_RT_BUILD_MEMPROF=OFF -DCOMPILER_RT_BUILD_ORC=OFF -DCOMPILER_RT_BUILD_PROFILE=OFF \
+						-DCOMPILER_RT_BUILD_SANITIZERS=OFF -DCOMPILER_RT_BUILD_XRAY=OFF -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON -DCOMPILER_RT_BUILD_STANDALONE_LIBATOMIC=ON"
+		_exec "Compiling libatomic" "cmake --build build/atomic -j$threads"
+		_exec "Installing libatomic" "cmake --install build/atomic --strip"
+		ln -sf "./linux/libclang_rt.atomic-$linux_arch.so" "$sysroot/lib/libatomic.so"
+	fi
 
-	# # libstdc++
-	# _compile_pkg "$sysroot/$libdir/libstdc++.so" "$gcc_dir" "" "" \
-	# 			"Compiling libstdc++" "" \
-	# 			"Installing libstdc++" "install-strip-target-libstdc++-v3"
+	# LLVM C++ Runtimes (libunwind, libcxxabi, pstl, and libcxx)
+	if [ ! -r "$sysroot/lib/libc++.so" ]; then
+		cd "$llvm_dir"
+		rm -rf "build/runtimes"
+		_exec "Configuring LLVM C++ Runtimes" "cmake -S ./runtimes -B build/runtimes $cmake_cross_flags -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
+						-DLLVM_ENABLE_RUNTIMES=libunwind\;libcxxabi\;pstl\;libcxx -DLIBUNWIND_USE_COMPILER_RT=ON -DLIBCXXABI_USE_COMPILER_RT=ON \
+						-DLIBCXX_USE_COMPILER_RT=ON -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DLIBCXXABI_HAS_CXA_THREAD_ATEXIT_IMPL=OFF -DLIBCXX_HAS_MUSL_LIBC=ON"
+		_exec "Compiling LLVM C++ Runtimes" "cmake --build build/runtimes -j$threads"
+		_exec "Installing LLVM C++ Runtimes" "cmake --install build/runtimes --strip"
+	fi
 
-	# # ncurses
+	# ncurses
 	# _compile_pkg "$sysroot/lib/libncurses.so" "$ncurses_dir" \
 	# 			"Configuring Ncurses" "--prefix=$sysroot --host=$compile_target --with-cxx-shared --with-shared --enable-overwrite --with-termlib" \
 	# 			"Compiling Ncurses" "" \
 	# 			"Installing Ncurses" "install INSTALL_PROG='/usr/bin/env install --strip-program=$compile_target-strip -c -s'"
 
-	# # ncursesw
+	# ncursesw
 	# if [ -r "$ncurses_dir/build" ]; then
 	# 	_exec "Cleaning Ncurses" "rm -rf $ncurses_dir/build"
 	# fi
