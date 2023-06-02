d67398c0b3d411f70c951e57a398adc9c3757f24
pocketlinux32 <pocketlinux32@gmail.com>
2023-06-02 04:19:50 -0500

    configure.rb can now download files :3

--- configure.rb
+++ configure.rb
@@ -8,7 +8,8 @@ $validToolchains = [ "llvm", "gcc" ]
 $arch = ""
 $toolchain = ""
 $prefix = File.expand_path("~/cross")
-$initDir = File.expand_path("pl-files/configure-files/")
+$baseDir = File.expand_path(".")
+$configDir = "#{$baseDir}/pl-files"
 
 def errorHandler(msg)
 	puts "Error: #{msg}. Run #{$0} -h for more information"
@@ -93,24 +94,77 @@ def validateArgs
 	end	
 end
 
-def fetchPkgs
-	pkgDir = Dir.open($initDir)
+def downloadFile(url, file, secure)
+	if File.exist?(file) == true
+		puts "#{File.basename(file)} exists, skipping..."
+		return
+	end
 
-	for i in pkgDir.each_child
-		fileParse = YAML.load_file($initDir + "/" + i)
-		pkg = fileParse.inspect
+	print "Downloading #{File.basename(file)}..."
+	
+	uri = URI(url)
+	port = 80
+	if secure == true
+		port = 443
+	end
 
-		print "Downloading #{pkg["name"]}..."
-		#
+	connection = Net::HTTP.new(uri.host, port)
+	connection.use_ssl = secure
+	response = connection.get(uri.path)
+	downloadedFile = File.open(file, "wb")
+	downloadedFile.write(response.body)
+	downloadedFile.close
+
+	puts "Done."
+end
+
+def fetchPkgs pkgList
+	for i in pkgList
+		fileParse = YAML.load_file("#{$configDir}/configure-files/pkg/" + i ".yaml")
+
+		if fileParse["github"] == true
+			tempUrl = "https://codeload.github.com/" + fileParse["url"] + "/tar.gz/refs/"
+			if fileParse["tag"] == nil
+				tempUrl = tempUrl + "heads/"
+				if fileParse["branch"] == nil
+					tempUrl = tempUrl + "main"
+				else
+					tempUrl = tempUrl + fileParse["branch"]
+				end
+			else
+				tempUrl = tempUrl + "tags/" + fileParse["tag"]
+			end
+
+			downloadFile(tempUrl, "#{$baseDir}/tarballs/" + fileParse["name"] + "-" + fileParse["version"] + ".tar.gz", true)
+		else
+			use_secure = false
+			if fileParse["secure"] != nil
+				use_secure = fileParse["secure"]
+			end
+
+			downloadFile(fileParse["url"], "#{$baseDir}/tarballs/" + File.basename(URI.parse(fileParse["url"]).path), use_secure)
+		end		
 	end
 end
 
 def init
-	fetchPkgs
+	if Dir.exist?("#{$baseDir}/tarballs") == false
+		Dir.mkdir("#{$baseDir}/tarballs")
+	end
+
+	puts "Stage 1: Download packages\n"
+	
+	presetFile = YAML.load_file("#{$configDir}/configure-files/" + $toolchain + ".yaml")
+	list = presetFile["pkgList"].scan(" ")
+
+	fetchPkgs list
+
+	puts "Stage 1 Complete! Starting Stage 2"
+	puts "Stage 2: Extract packages\n"
 end
 
-puts "PortaLinux Build System v0.11"
-print "(c) 2023 pocketlinux32 & raisinware, Under MPL 2.0\n\n"
+puts "PortaLinux Configure System v0.11"
+print "(c) 2022-2023 pocketlinux32 & raisinware, Under MPL 2.0\n\n"
 
 if ARGV.length < 1
 	errorHandler "Not enough arguments"
