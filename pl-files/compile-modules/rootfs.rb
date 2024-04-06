3fe22a78eb7683b37faa6e1c31a48297e9e7a5b4
raisin <raisinware@outlook.com>
2024-04-05 19:36:01 -0700

    enable xzcat
    enables xzcat in toybox so it can decompress xz files. also backports some toybox commits that haven't made it into a release yet

--- pl-files/compile-modules/rootfs.rb
+++ pl-files/compile-modules/rootfs.rb
@@ -47,7 +47,7 @@ def rootfsBuild globalVars
 		Dir.chdir("#{globalVars["buildDir"]}/toybox-#{globalVars["toybox"]}")
 		system("make defconfig 2>#{globalVars["baseDir"]}/logs/toybox-error.log >#{globalVars["baseDir"]}/logs/toybox.log")
 		configFile = File.open(".config", "a")
-		configFile.write("CONFIG_DD=y\nCONFIG_EXPR=y\nCONFIG_GETTY=y\nCONFIG_MDEV=y\nCONFIG_TOYBOX_LIBZ=y\n")
+		configFile.write("CONFIG_DD=y\nCONFIG_EXPR=y\nCONFIG_GETTY=y\nCONFIG_MDEV=y\nCONFIG_TOYBOX_LIBZ=y\nCONFIG_XZCAT=y\n")
 		configFile.close()
 		system("make -j#{globalVars["threads"]} CC=#{globalVars["tcprefix"]}/bin/#{globalVars["cross_cc"]} CFLAGS=#{globalVars["cross_cflags"]} 2>>#{globalVars["baseDir"]}/logs/toybox-error.log >>#{globalVars["baseDir"]}/logs/toybox.log")
 		puts "Done"
