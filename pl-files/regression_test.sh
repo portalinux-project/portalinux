280e4d025103c0a763b62cc851d05b6291534774
raisin <raisinware@outlook.com>
2024-05-05 11:34:58 -0700

    Create regression_test.sh
    this is a simple shell script that builds the toolchain and rootfs all the supported gcc arches. useful for making sure we dont break stuff in the future

---
+++ pl-files/regression_test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+
+TOOLCHAIN_DIR="$HOME/cross_test"
+GCC_ARCH_LIST="i486 i586 i686 x86_64 armv5 armv6 armv6k armv7 aarch64 riscv64"
+
+export PATH="$TOOLCHAIN_DIR/gcc/bin:$PATH"
+
+# cleanup from previous runs
+#rm -rf "$TOOLCHAIN_DIR/gcc/output"
+#rm -rf "$TOOLCHAIN_DIR/gcc/logs"
+
+# GCC tests
+for arch in $GCC_ARCH_LIST
+do
+	# toolchain
+	./configure.rb -p gcc -t "$TOOLCHAIN_DIR" -a $arch
+	./compile.rb -b toolchain
+	./compile.rb -b toolchain # try this a second time to make sure the lib64
+	                          # bug never resurfaces
+
+	# rootfs
+	mkdir -p "$TOOLCHAIN_DIR/gcc/output"
+	./compile.rb -b rootfs
+	mv output "$TOOLCHAIN_DIR/gcc/output/$arch"
+
+	# logs
+	mkdir -p "$TOOLCHAIN_DIR/gcc/logs"
+	mv logs "$TOOLCHAIN_DIR/gcc/logs/$arch"
+
+	./compile.rb -c 3
+done
